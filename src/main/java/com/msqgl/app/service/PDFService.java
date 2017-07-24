a1300af34d0f11c480aa85417c46d684315b46fe
Michele <squeglia.michele@gmail.com>
2017-07-24 12:09:21 +0200

    Added log info

--- src/main/java/com/msqgl/app/service/PDFService.java
+++ src/main/java/com/msqgl/app/service/PDFService.java
@@ -11,6 +11,8 @@ import com.msqgl.app.dao.GiftDao;
 import com.msqgl.app.dao.MsgDao;
 import com.msqgl.app.model.Gift;
 import com.msqgl.app.model.Msg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +29,8 @@ import java.util.List;
 
 @Service
 public class PDFService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PDFService.class);
 
   private static final int LOGO_WIDTH = 100;
 
@@ -107,6 +111,10 @@ public class PDFService {
       cell.setHorizontalAlignment(Element.ALIGN_CENTER);
       table.addCell(cell);
     }
+
+    LOG.info("Totale incassato: {}", total);
+    LOG.info("Media regalo: {}", total.divide(new BigDecimal(allMsg.size())));
+    LOG.info("Numero di regali: {}" + allMsg.size());
 
     Paragraph main = new Paragraph();
     main.setAlignment(Element.ALIGN_CENTER);
